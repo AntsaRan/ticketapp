7139135dc49fb0251a178f7523d336fb45b06a81
Antsa <vranari@yahoo.fr>
2023-07-28 18:22:32 +0300

    util service + toast

---
+++ src/app/shared/utils.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  //toast
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(private _snackBar: MatSnackBar) { }
+  
+  errorToast() {
+    this._snackBar.open('An error occured, please try again', 'ok', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+}
