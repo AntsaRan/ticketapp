ac083c7e4c50f29b477b64b383d6af2931af0c30
Antsa <vranari@yahoo.fr>
2023-07-28 13:22:21 +0300

    error handling

--- src/app/ticket-details/ticket-details.component.ts
+++ src/app/ticket-details/ticket-details.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Ticket } from 'src/interfaces/ticket.interface';
 import { MatCardModule } from '@angular/material/card'
-import { map, startWith, switchMap } from 'rxjs/operators';
+import { catchError, filter, map, mergeMap, startWith, switchMap } from 'rxjs/operators';
 import { NgFor, AsyncPipe } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
@@ -29,6 +29,8 @@ export class TicketDetailsComponent {
   isDataLoaded = false;
   isInputFocused = false;
   selectedUser: User;
+  isLoadingAssign = false;
+  ticketNotFound = false;
   private _filter(name: string): User[] {
     const filterValue = name.toLowerCase();
 
@@ -45,76 +47,76 @@ export class TicketDetailsComponent {
   }
   ngOnInit() {
     this.getTicket().subscribe(() => {
-      this.assigneeCtrl.setValue(this.user); // Set the initial value of the assigneeCtrl to the user object
-      this.filterusers = this.assigneeCtrl.valueChanges.pipe(
-        startWith(''),
-        map(value => {
-          const name = typeof value === 'string' ? value : value?.name;
-          return name ? this._filter(name as string) : this.users.slice();
-        })
-      );
-      this.isDataLoaded = true;
+      if (!this.ticket) {
+        this.ticketNotFound = true;
+      } else {
+        this.assigneeCtrl.setValue(this.user); // Set the initial value of the assigneeCtrl to the user object
+        this.filterusers = this.assigneeCtrl.valueChanges.pipe(
+          startWith(''),
+          map(value => {
+            const name = typeof value === 'string' ? value : value?.name;
+            return name ? this._filter(name as string) : this.users.slice();
+          })
+        );
+        this.isDataLoaded = true;
+      }
     });
   }
   displayFn(user: User): string {
     return user && user.name ? user.name : '';
   }
-  /* getTicket() {
-     return this.backendService.ticket(this.ticketId).pipe(
-       switchMap(ticket => {
-         this.ticket = ticket;
-         return this.backendService.user(ticket.assigneeId ? ticket.assigneeId : null);
-       })
-     ).pipe(
-       switchMap(user => {
-         this.user = user;
-         this.ticketUSer = new TicketUser(this.ticket.id, this.ticket.completed, user, this.ticket.description);
-         return this.backendService.users();
-       })
-     ).pipe(
-       map(users => {
-         this.users = users;
-       })
-     );
-   }*/
+
   getTicket(): Observable<void> {
-    return this.backendService.ticket(this.ticketId).pipe(
+    return of(this.ticketId).pipe(
+      switchMap(ticketId => {
+        if (!ticketId) {
+          return of(null);
+        }
+        return this.backendService.ticket(ticketId) ? this.backendService.ticket(ticketId) : of(null);
+      }),
       switchMap(ticket => {
+        if (!ticket) {
+          return of(null);
+        }
         this.ticket = ticket;
-        console.log(JSON.stringify(ticket) + " ticket")
-        return this.backendService.user(ticket.assigneeId ? ticket.assigneeId : null)
-          .pipe(
-            switchMap(user => {
-              this.user = user;
-              this.ticketUSer = new TicketUser(this.ticket.id, this.ticket.completed, user, this.ticket.description);
-              return this.backendService.users();
-            })
-          );
-      })
-    ).pipe(
+        return this.backendService.user(ticket.assigneeId ? ticket.assigneeId : null).pipe(
+          switchMap(user => {
+            this.user = user;
+            this.ticketUSer = new TicketUser(this.ticket.id, this.ticket.completed, user, this.ticket.description);
+            return this.backendService.users();
+          })
+        );
+      }),// Provide a default value (empty array) if no values are emitted
       map(users => {
         this.users = users;
       })
     );
   }
+
   completeTicket() {
-    this.backendService.complete1(this.ticket.id, true).subscribe(t => {
-      if (t) {
-        console.log(JSON.stringify(t) + "complete");
+    this.backendService.complete1(this.ticket.id, true).subscribe(ticketUpdated => {
+      if (ticketUpdated) {
+        console.log(JSON.stringify(ticketUpdated) + "complete");
         this.getTicket().subscribe(() => {
         })
       }
     })
   }
-  uncompleteTicket() { 
+  uncompleteTicket() {
 
   }
   assign() {
+    this.isLoadingAssign = true;
     if (this.selectedUser) {
-      this.backendService.assign1(this.ticket.id, this.selectedUser.id).subscribe(u => {
-        this.getTicket().subscribe(() => {
-          this.selectedUser = null;
-        });
+      this.backendService.assign1(this.ticket.id, this.selectedUser.id).subscribe(ticketUpdated => {
+        if (ticketUpdated) {
+          this.isLoadingAssign = false;
+          this.ticket = ticketUpdated;
+          this.getTicket().subscribe(() => {
+            this.selectedUser = null;
+          });
+        }
+
       });
     }
   }
