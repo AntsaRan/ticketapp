19b63483257b86af3318c9e6e65a9dd33690482a
Antsa <vranari@yahoo.fr>
2023-07-29 00:32:12 +0300

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,33 @@
 import { AppComponent } from './app.component';
-import { BackendService } from './backend.service';
 import { AddticketDialogComponent } from './addticket-dialog/addticket-dialog.component';
+import { BackendService } from './backend.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatCardModule} from '@angular/material/card';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { TicketDetailsComponent } from './ticket-details/ticket-details.component';
 import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { UtilsService } from './shared/utils.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ErrorToastComponent } from './error-toast/error-toast.component';
 
 const routes: Routes = [
     {
@@ -42,21 +41,32 @@ const routes: Routes = [
     }
 ]
 @NgModule({
-    declarations: [AppComponent, TicketDetailsComponent, TicketListComponent, AddticketDialogComponent, ErrorToastComponent],
+    declarations: [AppComponent, TicketDetailsComponent, TicketListComponent, AddticketDialogComponent],
     imports: [
-        BrowserModule, MatButtonModule,
-        MatFormFieldModule, MatCheckboxModule,
-        MatSidenavModule, MatToolbarModule,
-        RouterModule.forRoot(routes), BrowserAnimationsModule,
-        MatButtonModule, MatDividerModule, MatIconModule,
-        MatInputModule, MatSelectModule,
-        MatTableModule, MatProgressSpinnerModule,
-        MatDialogModule, FormsModule, MatProgressBarModule,
-        MatPaginatorModule,MatCardModule,
-        MatAutocompleteModule,ReactiveFormsModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes),
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatDividerModule,
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatTableModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatProgressBarModule,
+        MatPaginatorModule,
+        MatAutocompleteModule,
+        MatCardModule
     ],
 
-    providers: [BackendService],
+    providers: [BackendService,UtilsService,MatSnackBar],
     bootstrap: [AppComponent]
 })
 export class AppModule {
