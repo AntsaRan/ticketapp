5a348fab72c7dbc2afba5b3efdb0e2685999c2b2
Antsa <vranari@yahoo.fr>
2023-07-28 18:29:12 +0300

    error catching

--- src/app/addticket-dialog/addticket-dialog.component.ts
+++ src/app/addticket-dialog/addticket-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BackendService } from '../backend.service';
 import { Ticket } from 'src/interfaces/ticket.interface';
 import { MatDialogRef } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-addticket-dialog',
@@ -17,37 +18,41 @@ export class AddticketDialogComponent {
   addticketmessage = "";
   descriptionTicket: string = "";
   inprogress = false;
-  isFormValid =false;
+  isFormValid = false;
   constructor(private backendService: BackendService, private dialogRef: MatDialogRef<AddticketDialogComponent>
   ) {
 
   }
   add_ticket() {
     this.inprogress = true;
-    console.log(this.descriptionTicket + "description ticket");
     if (this.descriptionTicket != "") {
       const ticket: any =
       {
         description: this.descriptionTicket,
       };
-      this.backendService.newTicket1(ticket)
-        .subscribe(newticket => {
-          if (newticket) {
-            this.inprogress = !this.inprogress;
-            this.ticketIsAdded = true;
-            setTimeout(() => {
-              this.dialogRef.close({ event: this.ticketIsAdded });
-              this.ticketIsAdded = false;
-            }, 2000);
-          } else {
-            this.inprogress = !this.inprogress;
-            this.errorinsert = true;
-            setTimeout(() => {
-              this.errorinsert = false;
-              this.dialogRef.close({ event: this.ticketIsAdded });
-            }, 2000);
-          }
+      this.backendService.newTicket1(ticket).pipe(
+        catchError(error => {
+          console.error('Error fetching tickets:', error);
+          this.errorinsert = true;
+          return of(null);
         })
+      ).subscribe(newticket => {
+        if (newticket) {
+          this.inprogress = !this.inprogress;
+          this.ticketIsAdded = true;
+          setTimeout(() => {
+            this.dialogRef.close({ event: this.ticketIsAdded });
+            this.ticketIsAdded = false;
+          }, 1000);
+        } else {
+          this.inprogress = !this.inprogress;
+          this.errorinsert = true;
+          setTimeout(() => {
+            this.errorinsert = false;
+            this.dialogRef.close({ event: this.ticketIsAdded });
+          }, 1000);
+        }
+      })
     }
   }
 }
