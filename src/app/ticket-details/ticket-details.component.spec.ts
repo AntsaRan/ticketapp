918f7e6b90c5f7336252da6d3212e6df3381a090
Antsa <vranari@yahoo.fr>
2023-07-29 00:31:36 +0300

    test

--- src/app/ticket-details/ticket-details.component.spec.ts
+++ src/app/ticket-details/ticket-details.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TicketDetailsComponent } from './ticket-details.component';
 
 describe('TicketDetailsComponent', () => {
@@ -8,7 +8,9 @@ describe('TicketDetailsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TicketDetailsComponent]
+      declarations: [TicketDetailsComponent],
+      imports: [RouterTestingModule],
+
     });
     fixture = TestBed.createComponent(TicketDetailsComponent);
     component = fixture.componentInstance;
